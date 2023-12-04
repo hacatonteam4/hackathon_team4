e87478c498cee9f5b3f544e8ab1e7d3f6dbe46c8
Артём Мариненко <zalgan@mail.ru>
2023-12-05 01:10:55 +0300

    Эндпоинт plan/ - view, serializer

--- career_tracker_backend/api/v1/views.py
+++ career_tracker_backend/api/v1/views.py
@@ -1,7 +1,6 @@
 from rest_framework import generics
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
-from specialties.models import Course, Specialization
+from specialties.models import Course
 from .serializers import GetCoursesSprecialization
 # Create your views here.
 
@@ -10,13 +9,8 @@ class GetPlanStudent(generics.ListAPIView):
     '''Обработка запроса на получение курсов по специальности студента'''
 
     serializer_class = GetCoursesSprecialization
-    permission_classes = [IsAuthenticatedOrReadOnly]
 
     def get_queryset(self):
-        a = Course.objects.filter(
-            specialization__courses_specialization__course_students__student=self.request.user
-        )
-        print(a)
         return Course.objects.filter(
             specialization__courses_specialization__course_students__student=self.request.user
         )
