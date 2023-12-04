49f0b7adc321091863d96eb82b3cb753990b85a5
Артём Мариненко <zalgan@mail.ru>
2023-12-04 20:46:19 +0300

    Эндпоинт plan, view, serializer

--- career_tracker_backend/api/v1/urls.py
+++ career_tracker_backend/api/v1/urls.py
@@ -2,11 +2,14 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
+from .views import GetPlanStudent
+
 
 app_name = 'api'
 
 router = DefaultRouter()
 
 urlpatterns = [
+    path('plan/', GetPlanStudent.as_view()),
     path('', include(router.urls))
 ]
