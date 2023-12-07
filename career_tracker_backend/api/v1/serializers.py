2fef285700ddc37562761499e49d67d6d184b485
Артём Мариненко <zalgan@mail.ru>
2023-12-08 02:42:22 +0300

    Docstring, добавлено поле list_skills в админке для Спринтов

--- career_tracker_backend/api/v1/serializers.py
+++ career_tracker_backend/api/v1/serializers.py
@@ -79,6 +79,7 @@ class StatisticDirectionsSerializer(serializers.ModelSerializer):
 
 
 class GetCoursesSprecialization(serializers.ModelSerializer):
+    """Сериализатор для получения всех курсов по специальности студента"""
 
     class Meta:
         model = Course
@@ -114,6 +115,9 @@ class GetGradeDirectionDescriptionSerializator(serializers.ModelSerializer):
 
 
 class GetDirectionSerializer(serializers.ModelSerializer):
+    """Сериализатор для получения направлений специальности студента
+    с процентным отображением знаний"""
+
     percent_direction = serializers.SerializerMethodField()
 
     class Meta:
@@ -125,6 +129,7 @@ class GetDirectionSerializer(serializers.ModelSerializer):
         )
 
     def get_percent_direction(self, obj):
+        """Вычисление % знаний студента в рамках конкретного направления"""
         request = self.context.get('request')
         direction_skills_count = Skill.objects.filter(
             grades_digrections__direction=obj
